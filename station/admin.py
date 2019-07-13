1f3b39fdc29dc43e9a400d01d5a75e3fe94cf7b9
Maciej Rosiak <orzel1244@gmail.com>
2019-07-13 18:36:51 +0200

    [Added] Models
    [Added] Models:
        - Station
        - Sensor 
        - SensorData

--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from station.models import Station, Sensor, SensorData
+
+admin.site.register(Station)
+admin.site.register(Sensor)
+admin.site.register(SensorData)
+
--- station/models.py
+++ station/models.py
@@ -1,3 +1,28 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models as models
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=40)
+    city = models.CharField(max_length=85)  # That is the longest city name being owned by city in New Zealand :)
+
+    def __str__(self):
+        return self.name
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=255)
+    last_update = models.DateTimeField(editable=False, null=True)
+    station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='station')
+
+    def __str__(self):
+        return f'{self.station.name}: {self.name}'
+
+
+class SensorData(models.Model):
+    value = models.FloatField()
+    date = models.DateTimeField(auto_now=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='sensor')
+
+    def __str__(self):
+        return f'{self.name} = {self.value}'
