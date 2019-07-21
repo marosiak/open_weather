25b7e570d3cac4248ecc794d90be1db05e8617ec
Maciej Rosiak <orzel1244@gmail.com>
2019-07-21 21:49:14 +0200

    [Added] Station details [Changed] Dashboard base class from "View" to "TemplateView"

--- station/urls.py
+++ station/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from station.views import DashboardView, StationsView
+from station.views import DashboardView, StationsList, StationDetail
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
-    path('stations/', StationsView.as_view(), name='stations')
+    path('stations/', StationsList.as_view(), name='stations'),
+    path('stations/<int:pk>', StationDetail.as_view(), name='station-detail'),
 ]
--- station/views.py
+++ station/views.py
@@ -1,19 +1,25 @@
 # Create your views here.
-from django.shortcuts import render
-from django.views.generic import ListView
-from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateView
 
 from .models import Station
 
 
-class DashboardView(View):
-    def get(self, request):
-        return render(request, 'dashboard.html', {'is_dashboard': True})
+class DashboardView(TemplateView):
+    template_name = "dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_dashboard'] = True
+        return context
 
 
-class StationsView(ListView):
-
+class StationsList(ListView):
     template_name = 'stations.html'
     model = Station
     context_object_name = 'stations'
-    queryset = Station.objects.all()
+
+
+class StationDetail(DetailView):
+    template_name = 'station_detail.html'
+    model = Station
